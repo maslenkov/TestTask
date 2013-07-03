256596d99e14f063f4442daadd898b98a0c72151
maslenkov <maslenkov@i.ua>
2013-07-03 17:01:14 +0300

    Another checkpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @user = User.first
   end
+
+  def new
+    @user = User.new
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,13 +5,28 @@ describe 'User pages' do
   subject {page}
 
   describe 'sign up page' do
-    visit signup_path
-    fill_in 'name', with: 'Example User'
-    fill_in 'email', with: 'Example@example.example'
-    fill_in 'password', with: 'password'
-    fill_in 'password', with: 'password'
-    click_button 'Register'
-  #
+    before {visit signup_path}
+
+    let(:submit) {'Register'}
+
+    describe 'with invalid input' do
+      it 'should not create user' do
+        expect {click_button submit}.not_to change(User, 'count')
+      end
+    end
+
+    describe 'with valid input' do
+    #TODO: нужно добавить тест проверки инвайта
+      before do
+        fill_in 'name', with: 'Example User'
+        fill_in 'email', with: 'Example@example.example'
+        fill_in 'password', with: 'password'
+        fill_in 'password', with: 'password'
+      end
+      it 'should create a user' do
+        expect {click_button submit}.to change(User, :count).by(1)
+      end
+    end
   end
 
   #describe 'profile page' do
