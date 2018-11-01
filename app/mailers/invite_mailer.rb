9a8399587763c0c84083c11b323f018c6a3895d1
Eugene Maslenkov <yujiriwc@gmail.com>
2018-11-01 21:01:10 +0300

    Fix: update hardcoded email url

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,7 @@ class InviteMailer < ActionMailer::Base
   default from: 'yujiriwc@gmail.com'
 
   def invitation_email(email, invite)
-    @url = 'http://sleepy-reef-6054.herokuapp.com/signup/' + invite
+    @url = 'https://kodo-test-em.herokuapp.com/signup/' + invite
     mail(to: email, subject: 'Invite for TestTask application')
   end
 end
